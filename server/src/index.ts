3c91f9d86ad8b76105ec8d6e2e66d73423404db3
ngocdu <ngocdu171@gmail.com>
2022-03-02 16:43:25 +0200

    connect to postgresql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello world haha 123!');
+require('dotenv').config()
+import 'reflect-metadata'
+import express from 'express'
+import { createConnection } from 'typeorm'
+
+const main = async () => {
+    await createConnection({
+        type: 'postgres',
+        database: 'copdp01_db',
+        username: process.env.DB_USERNAME_DEV,
+        password: process.env.DB_PASSWORD_DEV,
+        logging: true,
+        synchronize: true
+    })
+
+    const app = express()
+    const port = 4000
+
+    app.listen(port, () => console.log(`Server started on port ${port}`))
+}
+
+main().catch(error => console.log(error))

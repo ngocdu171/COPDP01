73998e72c2ba1d88433ba72d8529cfdef0151184
ngocdu <ngocdu171@gmail.com>
2022-03-04 11:39:14 +0200

    define type of user after return value into graphql

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,22 +1,29 @@
+import { Field, ID, ObjectType } from "type-graphql";
 import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
+@ObjectType()
 @Entity()
 export class User extends BaseEntity {
+    @Field(_type => ID)
     @PrimaryGeneratedColumn()
     id!: number
 
+    @Field()
     @Column({ unique: true })
     username!: string
 
+    @Field()
     @Column({ unique: true })
     email!: string
 
     @Column()
     password!: string
 
+    @Field()
     @CreateDateColumn()
-    createdAt!: string
+    createdAt!: Date
 
+    @Field()
     @UpdateDateColumn()
-    updatedAt!: string
+    updatedAt!: Date
 }

4e1983dfd19a5f774081b39ca30c81c97bbf7e57
ngocdu <ngocdu171@gmail.com>
2022-03-10 19:20:49 +0200

    get session of user that send request

--- server/src/resolvers/hello.ts
+++ server/src/resolvers/hello.ts
@@ -1,9 +1,13 @@
-import { Query, Resolver } from "type-graphql";
+import { Context } from "../type/Context";
+import { Ctx, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class HelloResolver {
     @Query(_returns => String)
-    hello() {
+    hello(
+        @Ctx() {req}: Context
+    ) {
+        console.log(req.session.userId)
         return 'hello world'
     }
 }

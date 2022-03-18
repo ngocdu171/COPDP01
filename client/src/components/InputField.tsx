21152d1a0e754c1b60fb71114fef8bf105134319
ngocdu <ngocdu171@gmail.com>
2022-03-18 14:02:31 +0200

    create button show and hide password

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -10,18 +10,20 @@ interface InputFieldProps {
   name: string;
   label: string;
   placeholder: string;
+  type?: string
 }
 
 const InputField = (props: InputFieldProps) => {
   const [field, { error }] = useField(props);
   return (
-    <FormControl>
+    <FormControl isRequired>
       <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
       <Input
         onChange={field.onChange}
         value={field.value}
         id={field.name}
         placeholder={props.placeholder}
+        type={props.type}
       />
       {error && <FormErrorMessage>{error}</FormErrorMessage>}
     </FormControl>

8fa780c0228506ee6f41773c421fc1c10088f8dc
ngocdu <ngocdu171@gmail.com>
2022-03-22 13:45:37 +0200

    adjust code in mutationRegister

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8f0ef41d108309a5"; }
+/******/ 		__webpack_require__.h = function() { return "92db25d96a233238"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

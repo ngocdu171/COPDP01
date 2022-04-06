ee14deb051ac7ed859080ce784839737231f7850
ngocdu <ngocdu171@gmail.com>
2022-04-06 10:29:42 +0300

    function change password

--- client/src/pages/change-password.tsx
+++ client/src/pages/change-password.tsx
@@ -1,17 +1,28 @@
 import { Box, Button, Center } from '@chakra-ui/react'
 import { Form, Formik } from 'formik'
+import { useRouter } from 'next/router'
 import React from 'react'
 import InputField from '../components/InputField'
 import Wrapper from '../components/Wrapper'
 import { ChangePasswordInput, useChangePasswordMutation } from '../generated/graphql'
 
 const ChangePassword = () => {
+  const router = useRouter();
     const initialValues = {newPassword: ''}
     const [changePassword, {loading}] = useChangePasswordMutation();
     const onChangePasswordSubmit = async (values: ChangePasswordInput) => {
-      // await changePassword({
-      //   variables: {}
-      // })
+      if(router.query.userId && router.query.token) {
+        const response = await changePassword({
+          variables: {
+            userId: router.query.userId as string,
+            token: router.query.token as string,
+            changePasswordInput: values
+          }
+        })
+        console.log(response);
+        
+      }
+      
     }
   return (
     <Wrapper>

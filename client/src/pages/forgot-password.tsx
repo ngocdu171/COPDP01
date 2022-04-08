232526d4db71167c04b64954922807857419adc9
ngocdu <ngocdu171@gmail.com>
2022-04-08 10:28:19 +0300

    checkAuth

--- client/src/pages/forgot-password.tsx
+++ client/src/pages/forgot-password.tsx
@@ -1,7 +1,9 @@
 import {
   Box,
   Button,
-  Center
+  Center,
+  Flex,
+  Spinner
 } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import InputField from "../components/InputField";
@@ -10,41 +12,51 @@ import {
   ForgotPasswordInput,
   useForgotPasswordMutation
 } from "../generated/graphql";
+import { useCheckAuth } from "../utils/useCheckAuth";
 
 const ForgotPassword = () => {
+
+  const {data: authData, loading: authLoading} = useCheckAuth()
   const initialValues = { email: "" };
   const [forgotPassword, { loading, data }] = useForgotPasswordMutation();
   const onForgotPasswordSubmit = async (values: ForgotPasswordInput) => {
     await forgotPassword({ variables: { forgotPasswordInput: values } });
   };
-  
-  return (
-    <Wrapper>
-      <Formik initialValues={initialValues} onSubmit={onForgotPasswordSubmit}>
-        {({ isSubmitting }) => !loading && data ? <Box>Please check your inbox</Box> : (
-          <Form>
-            <InputField
-              name="email"
-              label="Email"
-              placeholder="Email"
-              type="email"
-            />
 
-            <Center>
-              <Button
-                type="submit"
-                colorScheme="blue"
-                mt={4}
-                isLoading={isSubmitting}
-              >
-                Send
-              </Button>
-            </Center>
-          </Form>
-        )}
-      </Formik>
-    </Wrapper>
-  );
+  if (authLoading || (!authLoading && authData?.checklogin)) {
+    return (
+      <Flex justifyContent='center' alignItems='center' minH='100vh'>
+        <Spinner />
+      </Flex>
+    )
+  } else
+    return (
+      <Wrapper>
+        <Formik initialValues={initialValues} onSubmit={onForgotPasswordSubmit}>
+          {({ isSubmitting }) => !loading && data ? <Box>Please check your inbox</Box> : (
+            <Form>
+              <InputField
+                name="email"
+                label="Email"
+                placeholder="Email"
+                type="email"
+              />
+
+              <Center>
+                <Button
+                  type="submit"
+                  colorScheme="blue"
+                  mt={4}
+                  isLoading={isSubmitting}
+                >
+                  Send
+                </Button>
+              </Center>
+            </Form>
+          )}
+        </Formik>
+      </Wrapper>
+    );
 };
 
 export default ForgotPassword;
--- client/src/utils/useCheckAuth.ts
+++ client/src/utils/useCheckAuth.ts
@@ -1,18 +1,25 @@
-import { useRouter } from "next/router"
-import { useEffect } from "react"
-import { useCheckLoginQuery } from "../generated/graphql"
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useCheckLoginQuery } from "../generated/graphql";
 
- export const useCheckAuth = () => {
-     const router = useRouter()
+export const useCheckAuth = () => {
+  const router = useRouter();
 
-     const {data, loading} = useCheckLoginQuery()
+  const { data, loading } = useCheckLoginQuery();
 
-     useEffect(() => {
-       if(!loading && data?.checklogin && (router.route === '/login' || router.route === '/register')) {
-           router.replace('/')
-       }
-     }, [loading, data, router])
-     return {
-         data, loading
-     }
- }
+  useEffect(() => {
+    if (
+      !loading &&
+      data?.checklogin &&
+      (router.route === "/login" ||
+        router.route === "/register" ||
+        router.route === "/forgot-password")
+    ) {
+      router.replace("/");
+    }
+  }, [loading, data, router]);
+  return {
+    data,
+    loading,
+  };
+};

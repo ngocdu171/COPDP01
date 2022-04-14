b9b5b7ff9db2fd4e9b84ad87342ad941769b0c35
ngocdu <ngocdu171@gmail.com>
2022-04-14 09:08:03 +0300

    Css navbar component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar = () => {
     }
   return (
     <Box p={5}>
-        <Flex justifyContent='space-between'>
+        <Flex maxW={800} justifyContent='space-between' align='center' m='auto'>
             <Heading><Link href={'/'}>S_Rent</Link></Heading>
             <Box>
                 {menuEle}
